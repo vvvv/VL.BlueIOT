6b4a9f939f498b735328eeebd8c2dd25f9d9d6b1
joreg <joreg@vvvv.org>
2024-04-18 14:55:21 +0200

    converted client project to net8

--- src/LsWebsocketInterface.cs
+++ src/LsWebsocketInterface.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text;
-using System.Threading;
 using WebSocketSharp;
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
 
 namespace LsWebsocketClient
 {
